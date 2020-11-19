5c4b509b5804891810e54904971122e416a9837e
Daan Buis <dbuis201@caledonian.ac.uk>
2020-11-19 09:22:36 +0000

    Person class first commit

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person
+{
+	private String firstName;
+	private String otherNames;
+	private String familyName;
+
+  public Person()
+  {
+	  firstName = "";
+	  otherNames = "";
+	  familyName = new Sring();
+  }
+
+}
